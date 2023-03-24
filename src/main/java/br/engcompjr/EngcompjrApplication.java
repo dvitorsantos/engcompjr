ed9f5c46630caaf91053316510bdea20276133c8
Vitor Santos <vitoralannl@gmail.com>
2023-03-23 21:14:41 -0300

    Initial commit

---
+++ src/main/java/br/engcompjr/EngcompjrApplication.java
@@ -0,0 +1,13 @@
+package br.engcompjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngcompjrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngcompjrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/engcompjr/EngcompjrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.engcompjr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngcompjrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
